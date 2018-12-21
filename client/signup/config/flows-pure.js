3b877db529f8e4f4648920f8cdbbe59fcae68cf0
fditrapani <filippo.di.trapani@automattic.com>
2018-12-21 14:26:16 -0500

    Genericizing Site Information Component to handle title, address, and phone steps independently. Moving `siteTitle` to the `siteInformation` object, updating tests.
    Updating siteTitle state to ensure consistency across journeys. To explain: the onboarding journey consolidates site information into title, address and phone (displayed on the site preview too), however since it's not the main journey, and we use the existing siteTitle state elsewhere, we'll maintain both until one or the other journey prevails.

--- client/signup/config/flows-pure.js
+++ client/signup/config/flows-pure.js
@@ -108,14 +108,23 @@ export function generateFlows( { getSiteDestination = noop, getPostsDestination 
 		},
 
 		onboarding: {
-			steps: [ 'user', 'site-type', 'site-topic', 'site-information', 'domains', 'plans' ],
+			steps: [ 'user', 'site-type', 'site-topic', 'site-information-title', 'domains', 'plans' ],
 			destination: getSiteDestination,
 			description: 'The improved onboarding flow.',
 			lastModified: '2018-10-22',
 		},
 
 		'onboarding-dev': {
-			steps: [ 'user', 'site-type', 'site-topic', 'site-information', 'domains', 'plans' ],
+			steps: [
+				'user',
+				'site-type',
+				'site-topic',
+				'site-information-title',
+				'site-information-address',
+				'site-information-phone',
+				'domains',
+				'plans',
+			],
 			destination: getSiteDestination,
 			description: 'A temporary flow for holding under-development steps',
 			lastModified: '2018-10-29',
